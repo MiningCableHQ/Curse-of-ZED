8b3ba2faa72459f7e27ef2e8c73af71382f64b48
sanjorjosharlene-hash <sanjorjosharlene@gmail.com>
2026-04-09 23:44:36 +0800

    updated world 3

--- src/Objects/AssetSetter.java
+++ src/Objects/AssetSetter.java
@@ -19,7 +19,10 @@ public class AssetSetter {
         if (gp.currentMap == 0) {
             new Map1Setter(gp).setObjects();
         } else if (gp.currentMap == 1) {
-            new Map2Setter(gp).setObjects();
+            Map2Setter map2  = new Map2Setter(gp);
+            map2.setObjectsPart1();
+            map2.setObjectsPart2();
+
         } else if (gp.currentMap == 2) {
             // This is where you call the new Map 3 class
             Map3Setter map3 = new Map3Setter(gp);
